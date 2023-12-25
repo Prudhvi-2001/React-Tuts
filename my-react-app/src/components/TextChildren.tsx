7e22ead822bd215865a9b3fe5334c6a7a4c44576
prudhvi <kuntrapakamprudhvi@gmail.com>
2023-12-25 14:58:05 +0530

    Promises are added

--- my-react-app/src/components/TextChildren.tsx
+++ my-react-app/src/components/TextChildren.tsx
@@ -1,6 +1,17 @@
 import React from 'react'
 
 function TextChildren({getText}) {
+    //creating the new object of the class Promise
+    const newPromise = new Promise((resolve,reject)=>{
+       resolve(12);
+       reject("Error")
+    })
+    newPromise.then(res =>{
+        console.log("Hi there !", res)
+    })
+    newPromise.catch(err =>{
+            console.error(err);
+    })
 
    getText("HI")
   return (
